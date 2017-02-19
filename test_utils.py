5d03b0e5121f6615464dfeda2c7ddad858f9418d
Jarno <ruedigerbusche@web.de>
2017-02-19 13:58:53 +0100

    Simplify and test Ranged types

---
+++ test_utils.py
@@ -0,0 +1,69 @@
+import unittest
+from utils import RangedNum, RangedInt
+
+
+class RangedNumTest(unittest.TestCase):
+
+    def setUp(self):
+        self.min = 1
+        self.max = 10
+        self.ranged_num = RangedNum(self.min, self.max)
+
+    def test_init(self):
+        self.assertTrue(self.min <= self.ranged_num <= self.max)
+
+    def test_add(self):
+        new_r = self.ranged_num + 5
+        self.assertIsInstance(new_r, RangedNum)
+        self.assertTrue(self.min <= new_r.value <= self.max)
+
+    def test_sub(self):
+        new_r = self.ranged_num - 5
+        self.assertIsInstance(new_r, RangedNum)
+        self.assertTrue(self.min <= new_r.value <= self.max)
+
+    def test_iadd(self):
+        self.ranged_num += 5
+        self.assertIsInstance(self.ranged_num, RangedNum)
+        self.assertTrue(self.min <= self.ranged_num.value <= self.max)
+
+    def test_isub(self):
+        self.ranged_num -= 5
+        self.assertIsInstance(self.ranged_num, RangedNum)
+        self.assertTrue(self.min <= self.ranged_num.value <= self.max)
+
+
+class RangedIntTest(unittest.TestCase):
+
+    def setUp(self):
+        self.min = 1
+        self.max = 10
+        self.ranged_int = RangedInt(self.min, self.max)
+
+    def test_init(self):
+        self.assertIsInstance(self.ranged_int.value, int)
+        self.assertIn(self.ranged_int.value, (i for i in range(self.min, self.max+1)))
+
+    def test_add(self):
+        new_r = self.ranged_int + 5
+        self.assertIsInstance(new_r, RangedInt)
+        self.assertIsInstance(new_r.value, int)
+        self.assertIn(new_r.value, (i for i in range(self.min, self.max+1)))
+
+    def test_sub(self):
+        new_r = self.ranged_int - 5
+        self.assertIsInstance(new_r, RangedInt)
+        self.assertIsInstance(new_r.value, int)
+        self.assertIn(new_r.value, (i for i in range(self.min, self.max+1)))
+
+    def test_iadd(self):
+        self.ranged_int += 5
+        self.assertIsInstance(self.ranged_int, RangedInt)
+        self.assertIsInstance(self.ranged_int.value, int)
+        self.assertIn(self.ranged_int.value, (i for i in range(self.min, self.max+1)))
+
+    def test_isub(self):
+        self.ranged_int -= 5
+        self.assertIsInstance(self.ranged_int, RangedInt)
+        self.assertIsInstance(self.ranged_int.value, int)
+        self.assertIn(self.ranged_int.value, (i for i in range(self.min, self.max+1)))
--- utils.py
+++ utils.py
@@ -1,45 +1,47 @@
 import random
 from json import JSONEncoder
 
+
 class RangedNum:
     """Keeps a numeric value in a certain range."""
 
-    def __init__(self, min, max, value="random"):
+    def __init__(self, min, max, value=None):
 
         self._min = min
         self._max = max
-        if value == "random":
-            self._value = random.uniform(self._min, self._max)
+        if value is None:
+            self.value = self._get_random()
         else:
-            self._value = value
+            # Use setter to ensure that value is in specified range.
+            self.value = value
 
     def __repr__(self):
         return '<RangedNum(min={}, max={}, value={})>'.format(self._min, self._max, self.value)
 
     def __add__(self, other):
-        return RangedNum(
+        return type(self)(
             self._min,
             self._max,
-            value=max(self._min, min(self._max, self._value + other))
+            value=self._value + other
         )
 
     def __sub__(self, other):
-        return RangedNum(
+        return type(self)(
             self._min,
             self._max,
-            value=max(self._min, min(self._max, self._value - other))
+            value=self._value - other
         )
 
     def __iadd__(self, other):
-        self._value = max(self._min, min(self._max, self._value + other))
+        self.value = self._value + other
         return self
 
     def __isub__(self, other):
-        self. value = max(self._min, min(self._max, self._value - other))
+        self.value = self._value - other
         return self
 
     def __eq__(self, other):
-        return (self._value == other)
+        return self._value == other
 
     @property
     def value(self):
@@ -49,51 +51,24 @@ class RangedNum:
     def value(self, value):
         self._value = max(self._min, min(value, self._max))
 
+    def _get_random(self):
+        return random.uniform(self._min, self._max)
 
-class RangedInt:
+
+class RangedInt(RangedNum):
     """Keeps an integer value in a certain range."""
-
-    def __init__(self, min, max, value="random"):
-
-        self._min = min
-        self._max = max
-        if value == "random":
-            self._value = random.randint(self._min, self._max)
-        else:
-            self._value = round(value)
 
     def __repr__(self):
         return '<RangedInt(min={}, max={}, value={})>'.format(self._min, self._max, self.value)
 
-    def __add__(self, other):
-        return RangedNum(
-            self._min,
-            self._max,
-            value=round(max(self._min, min(self._max, self._value + other)))
-        )
-
-    def __sub__(self, other):
-        return RangedNum(
-            self._min,
-            self._max,
-            value=round(max(self._min, min(self._max, self._value - other)))
-        )
-
-    def __iadd__(self, other):
-        self._value = round(max(self._min, min(self._max, self._value + other)))
-        return self
-
-    def __isub__(self, other):
-        self._value = round(max(self._min, min(self._max, self._value - other)))
-        return self
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
+    @RangedNum.value.setter
     def value(self, value):
-        self._value = max(self._min, min(value, self._max))
+        self._value = round(max(self._min, min(value, self._max)))
+
+    def _get_random(self):
+        return random.randint(self._min, self._max)
+
+
 
 
 class RangedJSONEncoder(JSONEncoder):
