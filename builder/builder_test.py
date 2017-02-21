f84aa8489af0f76cd506ffcbfb6525fa8768e94b
Jarno <ruedigerbusche@web.de>
2017-02-21 16:55:38 +0100

    Evaluate with 5000 data points

--- builder/builder_test.py
+++ builder/builder_test.py
@@ -1,6 +1,6 @@
 from builder.network_builder import Network
 
-with open('../test_ff.json') as fp:
+with open('../test_spec_simple.json') as fp:
     spec = str(fp.read())
     # print(spec)
     network = Network(spec)
