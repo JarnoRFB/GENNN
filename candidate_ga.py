5132b2fc0f80ab6abc87780cc675fa39a529fba0
elix <fbeering@gmail.com>
2017-02-22 16:55:11 +0100

    Hyperparameter optimizier Init

---
+++ candidate_ga.py
@@ -0,0 +1,13 @@
+
+class candidate_ga:
+    def __init__(self, candidate_id, start_time_str, runtime_spec, network_spec=None):
+        self._problem_class =runtime_spec
+    def crossover(self,crossover_parms, other_candidate):
+
+    def mutation(self, mutation_rate):
+
+    def get_diversity(self, other_candidate):
+
+    def get_fitness(self):
+
+    def to_next_generation(self, generation):
