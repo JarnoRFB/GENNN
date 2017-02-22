b9d399f605afab557002df023cf56fc21948fd54
Kampfkarotte <lniehaus92@gmail.com>
2017-02-22 19:44:25 +0100

    smal debugs

--- candidate_ga.py
+++ candidate_ga.py
@@ -4,12 +4,13 @@ import random
 from ga import GA
 from copy import deepcopy
 import os
+from utils import RangedJSONEncoder
 
 class Candidate_GA:
 
     def __init__(self, candidate_id, start_time_str, runtime_spec, network_spec=None):
         self._candidate_id = candidate_id
-        self._base_logdir = os.path.join(self.runtime_spec['logdir'], str(start_time_str))
+        self._base_logdir = os.path.join(runtime_spec['logdir'], str(start_time_str))
 
         self._problem_class = runtime_spec
         if network_spec is None:
@@ -32,18 +33,18 @@ class Candidate_GA:
 
     def _crossover_uniform(self, crossover_rate, other_candidate, uniform_method):
         if random.uniform(0, 1) <= crossover_rate:
-            self.network_spec['crossover']['rate'] = other_candidate._hyper_parms['crossover']['rate']
+            self.network_spec['crossover']['rate'] = other_candidate.network_spec['crossover']['rate']
 
         if random.uniform(0, 1) <= crossover_rate:
-            self.network_spec['mutation']['rate'] = other_candidate._hyper_parms['mutation']['rate']
+            self.network_spec['mutation']['rate'] = other_candidate.network_spec['mutation']['rate']
 
         if random.uniform(0, 1) <= crossover_rate:
             self.network_spec['selection_strategy']['best_win_rate'] = \
-            other_candidate._hyper_parms['selection_strategy']['best_win_rate']
+            other_candidate.network_spec['selection_strategy']['best_win_rate']
 
         if random.uniform(0, 1) <= crossover_rate:
             self.network_spec['selection_strategy']['tournament_size'] = \
-            other_candidate._hyper_parms['selection_strategy']['tournament_size']
+            other_candidate.network_spec['selection_strategy']['tournament_size']
 
     def mutation(self, mutation_rate):
         self._fitness = None
@@ -88,27 +89,28 @@ class Candidate_GA:
                 gen.crossover(strategy=self.network_spec['crossover'])
                 gen.evaluate(calc_diversity=self.network_spec['calc_diversity'])
                 gen.selection()
-                if gen.best_candidate_forever.get_fitness() >= self._best_candidate.get_fitness():
+                if self._best_candidate is None or gen.best_candidate_forever.get_fitness() >= self._best_candidate.get_fitness():
                     self._best_candidate = deepcopy(gen.best_candidate_forever)
                 print("-Gen: "+str(gen.generation)+"-Diversity: "+str(round(gen.diversity))+"- Fitness_avg: "+str(round(gen.fitness_avg,3))+"- Fitness_best: "+str(round(gen.best_candidate.get_fitness(),3)))
-                self._fitness = 0.6*gen.best_candidate + 0.4*gen.fitness_avg
+                self._fitness = 0.6*gen.best_candidate.get_fitness() + 0.4*gen.fitness_avg
 
         file_loc = os.path.join(self.network_spec['RUNTIME_SPEC']['logdir'], "ga.json")
         with open(file_loc, 'w') as fp:
-            fp.write(str(self._parms))
+            fp.write(str(self._fitness)+"\n")
+            fp.write(RangedJSONEncoder().encode(self.network_spec))
         return self._fitness
 
     def to_next_generation(self, generation):
         self.outer_gen = generation
         generation_dir = 'generation_{}/'.format(generation)
         id_dir = '{}/'.format(self._candidate_id)
-        self.runtime_spec['logdir'] = os.path.join(self._base_logdir, generation_dir, id_dir)
-        self.network_spec.update(self.runtime_spec)
+        self.network_spec['RUNTIME_SPEC']['logdir'] = os.path.join(self._base_logdir, generation_dir, id_dir)
+        #self.network_spec.update(self.network_spec)
 
     outer_gen = 0
     def _generate_random(self):
         hyper_parms = genetic_hyperparamter = {
-            'population': 10,
+            'population': 2,
             'calc_diversity': False,
             'crossover': {
                 'strategy': 'uniform_crossover',
@@ -120,7 +122,7 @@ class Candidate_GA:
 
             },
             'candidate_class': CandidateNN,
-            'number_of_generation': 10,
+            'number_of_generation': 1,
             'selection_strategy': {
                 'type': 'Tournament',
                 'best_win_rate': RangedNum(0.5, 0.9).value,
